1e004117e18d4b9830b9d0b0c3132b3e7b8312a9
MoParnassus <56222484+MoParnassus@users.noreply.github.com>
2020-03-21 09:25:07 +0100

    Signed-off-by: MoParnassus <56222484+MoParnassus@users.noreply.github.com>

---
+++ LABIV_1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace LABIV_1
+{
+    class CURD
+    {
+        public void Create(int ID,string kategorie, SqlConnection conn)
+        {
+            string sql = "INSERT INTO Kategorie('kategorie') VALUES (@NazwaKategorii)";
+            var command = new SqlConnection(sql, conn);
+            command.Parameters.AddWithValue("@NazwaKategorii", kategorie);
+            command.Parameters.AddWithValue("@ID", ID);
+
+            int x = command.Execute;
+        }
+    }
+}
---
+++ LABIV_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LAB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=DESKTOP-H7IC758\TEST;Initial Catalog=ZNorthwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "SELECT * FROM Klienci",
+                Connection = conn
+            };
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["NazwaFirmy"]);
+            }
+
+            conn.Close();
+        }
+    }
+}
